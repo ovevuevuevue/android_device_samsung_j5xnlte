0953324d7c8bf9a83e9e627b468d8b61c5a2c102
mikotoMisaka <70590405+ovevuevuevue@users.noreply.github.com>
2021-12-25 23:10:26 +0100

    Update omni_j5xnlte.mk

--- omni_j5xnlte.mk
+++ omni_j5xnlte.mk
@@ -1,6 +1,4 @@
 # Inherit from common
-$(call inherit-product, device/samsung/j5x-common/omni.mk)
-
 $(call inherit-product, device/samsung/j5xnlte/device.mk)
 
 ## Device identifier. This must come after all inclusions
